79d65aefb71f1b826faa52fdd8d9b459ad56afed
Jonathan Black <apps2black@gmail.com>
2014-05-31 22:55:46 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jonpinteresting::Application.config.secret_key_base = '9a276e3a5520c26a4b87a3c522e8d14b89f651f5762efe056fdcaee5afcc1d333ca9c9f27ea70bb0e3430a7269b4d0b284df9fa55d910d13441b5c64f7966a11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jonpinteresting::Application.config.session_store :cookie_store, key: '_jonpinteresting_session'
