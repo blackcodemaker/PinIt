14e7a3df7b641e74bb5f0aa70ca19e638cfd20e4
Jonathan Black <apps2black@gmail.com>
2015-04-26 14:47:48 -0700

    Implemented devise fo users

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jquery-rails', '3.0.4'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
 gem 'bootstrap-sass'
+gem 'devise', "~> 3.4.1"
 
 group :doc do
   gem 'sdoc', '0.3.20', require: false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Jonpinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+    #Required for HEroku
+  config.action_mailer.default_url_options = { host => 'pin-it.herokuapp.com' }
 end
